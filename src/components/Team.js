fab623ef01439578730ed60c0647af3c90f08d76
Jeffrey Rujen R <jeffrey.rujen.r@sap.com>
2023-05-12 17:57:09 +0530

    team in progress

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,81 +1,46 @@
 import Image from "next/image";
 
+const team = [
+  {
+    image: "",
+    name: "Name 1",
+    role: "Role 1",
+  },
+  {
+    image: "",
+    name: "Name 2",
+    role: "Role 2",
+  },
+  {
+    image: "",
+    name: "Name 3",
+    role: "Role 3",
+  },
+];
+
 const Team = () => {
   return (
-    <section className="flex flex-col h-screen justify-center items-center text-center">
+    <section className="flex flex-col h-screen w-full justify-center items-center text-center">
       <h1 className="text-3xl mb-8">Our Team</h1>
-      {/* <div className="flex flex-col justify-around items-center text-center">
-        <div className="flex justify-center items-center p-8 border-2 rounded-lg backdrop-blur-lg">
-          <Image
-            className="rounded-lg bg-black mr-4"
-            src=""
-            alt=""
-            height={72}
-            width={72}
-          />
-          <div className="flex flex-col justify-center items-center text-center">
-            <h3 className="text-xl opacity-75">Lorem ipsum dolor sit</h3>
-            <p className="opacity-50">Lorem ipsum dolor</p>
+      <div className="flex w-full justify-evenly items-center text-center">
+        {team.map((member) => (
+          <div
+            key={member.name}
+            className="card w-1/3 mx-4 bg-base-100 shadow-xl"
+          >
+            <Image
+              src={member.image}
+              alt=""
+              width={150}
+              height={150}
+              className="mx-auto mt-8 bg-white rounded-xl"
+            />
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">{member.name}</h2>
+              <p>{member.role}</p>
+            </div>
           </div>
-        </div>
-        <div className="flex justify-center items-center p-8 border-2 rounded-lg backdrop-blur-lg">
-          <Image
-            className="rounded-lg bg-black mr-4"
-            src=""
-            alt=""
-            height={72}
-            width={72}
-          />
-          <div className="flex flex-col justify-center items-center text-center">
-            <h3 className="text-xl opacity-75">Lorem ipsum dolor sit</h3>
-            <p className="opacity-50">Lorem ipsum dolor</p>
-          </div>
-        </div>
-        <div className="flex justify-center items-center p-8 border-2 rounded-lg backdrop-blur-lg">
-          <Image
-            className="rounded-lg bg-black mr-4"
-            src=""
-            alt=""
-            height={72}
-            width={72}
-          />
-          <div className="flex flex-col justify-center items-center text-center">
-            <h3 className="text-xl opacity-75">Lorem ipsum dolor sit</h3>
-            <p className="opacity-50">Lorem ipsum dolor</p>
-          </div>
-        </div>
-      </div> */}
-      <div className="flex justify-evenly items-center text-center">
-        <div className="card w-1/3 bg-base-100 shadow-xl">
-          <Image src="" alt="" className="px-10 pt-10" />
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">Lorem ipsum dolor sit</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              voluptatum.
-            </p>
-          </div>
-        </div>
-        <div className="card w-1/3 bg-base-100 shadow-xl">
-          <Image src="" alt="" className="px-10 pt-10" />
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">Lorem ipsum dolor sit</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              voluptatum.
-            </p>
-          </div>
-        </div>
-        <div className="card w-1/3 bg-base-100 shadow-xl">
-          <Image src="" alt="" className="px-10 pt-10" />
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">Lorem ipsum dolor sit</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              voluptatum.
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
