f4308e6e8d4b25c54098296dff118495fd731c86
Jeffrey Rujen R <jeffrey.rujen.r@sap.com>
2023-05-19 14:57:18 +0530

    images update 2

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,41 +1,42 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 const courses = [
-	{
-		image: '/premiere pro.png',
-		name: 'Learn Premiere Pro: Beginner level',
-		instructor: 'Preethi Ravichandran',
-		duration: '3 months',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		enroll:
-			'https://docs.google.com/forms/d/e/1FAIpQLSdbtkRC3bNEW7QFW-26G2Op3DkdiHhbfrcWeViphNFT7Zt5Ig/viewform',
-	},
-	{
-		image: '/photoshop.png',
-		name: 'Learn Photoshop: Beginner level',
-		instructor: 'Sushmita Selvadass',
-		duration: '3 months',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		enroll:
-			'https://docs.google.com/forms/d/e/1FAIpQLSenAcZtNmZNF3YEI3Tc4u9EtA2FL6ToV_vFFbBDQSVpUoy0bw/viewform',
-	},
-	{
-		image: '/entrepreneurship.jpg',
-		name: 'Diploma in Entrepreneurship & Leadership',
-		instructor: 'Prabakar Mariyaraj',
-		duration: '1 year',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-	},
+  {
+    image: "/premiere pro.png",
+    name: "Learn Premiere Pro: Beginner level",
+    instructor: "Preethi Ravichandran",
+    duration: "3 months",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    enroll:
+      "https://docs.google.com/forms/d/e/1FAIpQLSdbtkRC3bNEW7QFW-26G2Op3DkdiHhbfrcWeViphNFT7Zt5Ig/viewform",
+  },
+  {
+    image: "/photoshop.png",
+    name: "Learn Photoshop: Beginner level",
+    instructor: "Sushmita Selvadass",
+    duration: "3 months",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    enroll:
+      "https://docs.google.com/forms/d/e/1FAIpQLSenAcZtNmZNF3YEI3Tc4u9EtA2FL6ToV_vFFbBDQSVpUoy0bw/viewform",
+  },
+  {
+    image: "/entrepreneurship.jpg",
+    name: "Diploma in Entrepreneurship & Leadership",
+    instructor: "Prabakar Mariyaraj",
+    duration: "1 year",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
 ];
 
 const Courses = () => {
-	return (
-		<section
-			id='courses'
-			className='flex flex-col min-h-screen w-full justify-center items-center text-center py-16'>
-			<h1 className='text-3xl'>Courses</h1>
-			{/* <div className='flex'>
+  return (
+    <section
+      id="courses"
+      className="flex flex-col min-h-screen w-full justify-center items-center text-center py-16"
+    >
+      <h1 className="text-3xl">Courses</h1>
+      {/* <div className='flex'>
 				<div className='flex flex-col justify-center items-center p-8 mx-4 rounded-lg backdrop-blur-lg'>
 					<Image
 						src=''
@@ -79,45 +80,49 @@ const Courses = () => {
 					</button>
 				</div>
 			</div> */}
-			<div className='flex flex-wrap justify-center items-center'>
-				{courses.map((course) => (
-					<div
-						key={course.name}
-						className='card w-96 shadow-xl m-4'>
-						<figure>
-							<Image
-								src={course.image}
-								alt=''
-								height={300}
-								width={400}
-								className='bg-white'
-							/>
-						</figure>
-						<div className='card-body border-x-[1px] border-b-[1px] rounded-2xl rounded-t-none bg-white/50'>
-							<h2 className='card-title'>
-								{course.name}
-								<div className='badge badge-secondary'>NEW</div>
-							</h2>
-							<p>{course.description}</p>
-							<div className='card-actions justify-evenly'>
-								<div className='badge badge-success'>{course.duration}</div>
-								<div className='badge badge-accent'>{course.instructor}</div>
-								<Link
-									href={course.enroll ? course.enroll : ''}
-									className={`btn ${
-										course.enroll
-											? 'btn-primary'
-											: 'btn-disabled backdrop-blur-xl'
-									}`}>
-									{course.enroll ? 'Register' : 'Coming Soon'}
-								</Link>
-							</div>
-						</div>
-					</div>
-				))}
-			</div>
-		</section>
-	);
+      <div className="flex flex-wrap justify-center items-center">
+        {courses.map((course) => (
+          <div
+            key={course.name}
+            className="card w-96 shadow-xl m-4 bg-white/50 backdrop-blur-xl"
+          >
+            <figure>
+              <Image
+                src={course.image}
+                alt=""
+                height={300}
+                width={400}
+                className="bg-white"
+                quality={100}
+                priority
+              />
+            </figure>
+            <div className="card-body border-x-[1px] border-b-[1px] rounded-2xl rounded-t-none bg-white/50">
+              <h2 className="card-title">
+                {course.name}
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>{course.description}</p>
+              <div className="card-actions justify-evenly">
+                <div className="badge badge-success">{course.duration}</div>
+                <div className="badge badge-accent">{course.instructor}</div>
+                <Link
+                  href={course.enroll ? course.enroll : ""}
+                  className={`btn ${
+                    course.enroll
+                      ? "btn-primary"
+                      : "btn-disabled backdrop-blur-xl"
+                  }`}
+                >
+                  {course.enroll ? "Register" : "Coming Soon"}
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Courses;
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,53 +1,56 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const team = [
-	{
-		image: '/Preethi.jpg',
-		name: 'Preethi Ravichandran',
-		role: 'Video Editing Coach',
-	},
-	{
-		image: '/Sushmita.jpg',
-		name: 'Sushmitha Selvadass',
-		role: 'Graphic Design Coach',
-	},
-	{
-		image: 'http://via.placeholder.com/360x360',
-		name: 'Prabakar Mariyaraj',
-		role: 'Entrepreneurship Coach',
-	},
+  {
+    image: "/Preethi.jpg",
+    name: "Preethi Ravichandran",
+    role: "Video Editing Coach",
+  },
+  {
+    image: "/Sushmita.jpg",
+    name: "Sushmitha Selvadass",
+    role: "Graphic Design Coach",
+  },
+  {
+    image: "http://via.placeholder.com/360x360",
+    name: "Prabakar Mariyaraj",
+    role: "Entrepreneurship Coach",
+  },
 ];
 
 const Team = () => {
-	return (
-		<section
-			id='team'
-			className='flex flex-col min-h-screen w-full justify-center items-center text-center py-16'>
-			<h1 className='text-3xl'>Our Team</h1>
-			<div className='flex flex-wrap w-full justify-evenly items-center'>
-				{team.map((member) => (
-					<div
-						key={member.name}
-						className='card lg:card-side shadow-xl w-96 my-4'>
-						<figure>
-							<Image
-								src={member.image}
-								alt=''
-								width={360}
-								height={360}
-								className='rounded bg-white'
-								quality={100}
-							/>
-						</figure>
-						<div className='card-body my-auto'>
-							<h2 className='card-title mx-auto'>{member.name}</h2>
-							<p className='mx-auto opacity-75'>{member.role}</p>
-						</div>
-					</div>
-				))}
-			</div>
-		</section>
-	);
+  return (
+    <section
+      id="team"
+      className="flex flex-col min-h-screen w-full justify-center items-center text-center py-16"
+    >
+      <h1 className="text-3xl">Our Team</h1>
+      <div className="flex flex-wrap w-full justify-evenly items-center">
+        {team.map((member) => (
+          <div
+            key={member.name}
+            className="card lg:card-side shadow-xl w-96 my-4 bg-white/50 backdrop-blur-xl"
+          >
+            <figure>
+              <Image
+                src={member.image}
+                alt=""
+                width={360}
+                height={360}
+                className="rounded bg-white"
+                quality={100}
+                priority
+              />
+            </figure>
+            <div className="card-body my-auto">
+              <h2 className="card-title mx-auto">{member.name}</h2>
+              <p className="mx-auto opacity-75">{member.role}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Team;
